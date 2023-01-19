3782849843d45cac897365c24a7af70d7a0f36f2
Eric Elsner <ericelsnercodes@gmail.com>
2023-01-19 09:43:38 -0500

    setup card structure in js and initialize basic styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 // console.log('winter is coming') // test log
+const charactersArray = []
+
 
 // card structure
+
+const card = document.createElement('div')
+card.className = 'card'
+const characterName = document.createElement('h2')
+characterName.className = 'character-name'
+// innertext will be characters called by search field
+const characterImage = document.createElement('img')
+characterImage.className = 'character-image'
+const buttonContainer = document.createElement('div')
+buttonContainer.className = 'button-container'
+const bioButton = document.createElement('button')
+bioButton.className = 'button'
+bioButton.innerText = 'Bio'
+const statusButton = document.createElement('button')
+statusButton.className = 'button'
+statusButton.innerText = 'Status'
