34edd0e84b0fe8d90d30118a12c3bb08ea9441cb
Eric Elsner <ericelsnercodes@gmail.com>
2023-01-20 09:48:01 -0500

    lol

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
 // console.log('winter is coming') // test log
-const charactersArray = []
 
+const charactersArray = [
+    {
+        name: 'Rhaenyra Targaryen',
+        bio: 'kisjudhfkjshd',
+        alive: true
+    }, 
+    {
+        name: 'Alicent Hightower',
+        bio: 'kisjudhfkjshd',
+        alive: true
+    }, 
+    {
+        name: 'King Viserys',
+        bio: 'kisjudhfkjshd',
+        alive: true
+    }, 
+    {
+        name: 'Daemon Targaryen',
+        bio: 'kisjudhfkjshd',
+        alive: true
+    }, 
+    {
+        name: 'Laenor Velaryon',
+        bio: 'kisjudhfkjshd',
+        alive: true
+    }, 
+    {
+        name: 'Laena Velaryon',
+        bio: 'kisjudhfkjshd',
+        alive: true
+    }
+]
 
 // card structure
 
-const card = document.createElement('div')
+const card = document.createElement('li')
 card.className = 'card'
 const characterName = document.createElement('h2')
 characterName.className = 'character-name'
@@ -19,3 +50,26 @@ bioButton.innerText = 'Bio'
 const statusButton = document.createElement('button')
 statusButton.className = 'button'
 statusButton.innerText = 'Status'
+
+const cards = document.getElementById('cards-container')
+
+const drawCard = (el) => {
+    cards.appendChild(card)
+    card.appendChild(characterName)
+    characterName.innerText = el.name
+    card.appendChild(buttonContainer)
+    buttonContainer.appendChild(bioButton)
+    buttonContainer.appendChild(statusButton)
+}
+charactersArray.map(el => { 
+    console.log(el)
+    return drawCard(el)
+    // cards.appendChild(card)
+    // card.appendChild(characterName)
+    // characterName.innerText = el.name
+    // card.appendChild(buttonContainer)
+    // buttonContainer.appendChild(bioButton)
+    // buttonContainer.appendChild(statusButton)
+})
+
+
